a1800cdde41c989c1bd78477c077894ed4bfd28d
Kamil Kucharczyk <kamil.kucharczyk007@gmail.com>
2024-01-24 16:37:25 +0100

    path correction

--- contacts.js
+++ contacts.js
@@ -1,7 +1,7 @@
 import colors from "colors";
 import fs from "fs";
 import path from "path";
-const contactsPath = path.join(process.cwd, "/db/contacts.json");
+const contactsPath = path.join(process.cwd(), "/db/contacts.json");
 
 function getContacts() {
   const contacts = fs.readFileSync(contactsPath, "utf8");
